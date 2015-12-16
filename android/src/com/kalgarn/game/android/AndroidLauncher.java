7dbdf36d8f248053bb0a7b03961c9496291ef086
Kalgarn <kalgarn@tuta.io>
2015-12-16 20:49:05 +0100

    #1 commit

---
+++ android/src/com/kalgarn/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kalgarn.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kalgarn.game.FlappyGDX;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyGDX(), config);
+	}
+}
---
+++ desktop/src/com/kalgarn/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kalgarn.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kalgarn.game.FlappyGDX;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyGDX(), config);
+	}
+}
