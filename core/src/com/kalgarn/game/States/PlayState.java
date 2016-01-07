33f9e8ac127a11fd3a0993e0e2b6bc6867828ec2
Kalgarn <kalgarn@tuta.io>
2016-01-07 13:28:32 +0100

    add score

--- core/src/com/kalgarn/game/States/PlayState.java
+++ core/src/com/kalgarn/game/States/PlayState.java
@@ -3,6 +3,7 @@ package com.kalgarn.game.States;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
@@ -28,6 +29,9 @@ public class PlayState extends State {
     private Vector2 groundPosition1;
     private Vector2 groundPosition2;
 
+    private int score;
+    private BitmapFont font;
+
     public PlayState(GameStateManager gsm) {
         super(gsm);
        // bird = new Texture("bird.png");
@@ -44,6 +48,9 @@ public class PlayState extends State {
         ground = new Texture("ground.png");
         groundPosition1 = new Vector2(cam.position.x - cam.viewportWidth /2 ,GROUND_Y_OFFSET);
         groundPosition2 = new Vector2((cam.position.x - cam.viewportWidth/2) + ground.getWidth(),GROUND_Y_OFFSET);
+
+        score = 0;
+
     }
 
     @Override
@@ -65,6 +72,7 @@ public class PlayState extends State {
         for (Tube tube : tubes){
             if(cam.position.x - (cam.viewportWidth / 2) > tube.getPositionTopTube().x + tube.getTopTube().getWidth()){
                 tube.reposition(tube.getPositionTopTube().x  + ((Tube.TUBE_WIDTH + TUBE_SPACING) * TUBE_COUNT));
+                score++;
             }
                 // reinitialise la game si collision
             if(tube.collides(bird.getPlayer())){
@@ -74,14 +82,17 @@ public class PlayState extends State {
                 gsm.set(new GameOver(gsm));
             }
                 // -- si touche le sol
-            if(bird.getPosition().y <= ground.getHeight() + GROUND_Y_OFFSET)
+            if(bird.getPosition().y <= ground.getHeight() + GROUND_Y_OFFSET) {
                 gsm.set(new MenuState(gsm));
+            }
         }
         cam.update();
+
     }
 
     @Override
     public void render(SpriteBatch sb) {
+        font = new BitmapFont();
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
         //sb.draw(bird, 50, 50);
@@ -93,14 +104,17 @@ public class PlayState extends State {
         }
         sb.draw(ground, groundPosition1.x,groundPosition1.y);
         sb.draw(ground, groundPosition2.x,groundPosition2.y);
+        font.draw(sb, "" + score, cam.position.x ,100);
         sb.end();
     }
 
     private void updateGround(){
-        if(cam.position.x - (cam.viewportWidth / 2) > groundPosition1.x + ground.getWidth())
+        if(cam.position.x - (cam.viewportWidth / 2) > groundPosition1.x + ground.getWidth()) {
             groundPosition1.add(ground.getWidth() * 2, 0);
-        if(cam.position.x - (cam.viewportWidth / 2) > groundPosition2.x + ground.getWidth())
+        }
+        if(cam.position.x - (cam.viewportWidth / 2) > groundPosition2.x + ground.getWidth()) {
             groundPosition2.add(ground.getWidth() * 2, 0);
+        }
     }
 
     @Override
