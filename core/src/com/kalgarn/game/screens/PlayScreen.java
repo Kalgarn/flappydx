a60099118b2431b0185f3f37c3704d6b3d716d72
Kalgarn <kalgarn@tuta.io>
2016-01-20 12:07:15 +0100

    fix stage #2

--- core/src/com/kalgarn/game/screens/PlayScreen.java
+++ core/src/com/kalgarn/game/screens/PlayScreen.java
@@ -12,7 +12,6 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.kalgarn.game.FlappyGDX;
 import com.kalgarn.game.HighScore;
 import com.kalgarn.game.objects.Bird;
@@ -65,7 +64,7 @@ public class PlayScreen implements Screen {
 
         font = new BitmapFont(Gdx.files.internal("font/font.fnt"),Gdx.files.internal("font/font.png"), false);
 
-        stage = new Stage(new StretchViewport(FlappyGDX.WIDTH, FlappyGDX.HEIGHT));
+        stage = new Stage();
         Table table = new Table();
         table.bottom().padBottom(15);
         table.setFillParent(true);
@@ -138,8 +137,6 @@ public class PlayScreen implements Screen {
         }
         game.batch.draw(ground, groundPosition1.x, groundPosition1.y);
         game.batch.draw(ground, groundPosition2.x, groundPosition2.y);
-        //font.draw(game.batch, "" + score, cam.position.x, 35);
-        //stage.draw();
         game.batch.end();
         stage.act();
         stage.draw();
